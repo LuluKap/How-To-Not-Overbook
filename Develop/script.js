2d15d5bed4af5899e05c6d50f88f58d7c7992aa9
Lulu Kapita <lulukapita@gmail.com>
2022-07-04 22:21:40 -0400

    Local Storage Attempt

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,26 @@ setInterval (() => {
 // time block colors
 //stackoverflow ftw
 
+$("#9oc").val(localStorage.a)
+$("#10oc").val(localStorage.b)
+$("#11oc").val(localStorage.c)
+$("#12oc").val(localStorage.d)
+$("#13oc").val(localStorage.e)
+$("#14oc").val(localStorage.f)
+$("#15oc").val(localStorage.g)
+$("#16oc").val(localStorage.h)
+$("#17oc").val(localStorage.i)
+
+$(".btn9").on("click", saveText);
+$(".btn10").on("click", saveText);
+$(".btn11").on("click", saveText);
+$(".btn12").on("click", saveText);
+$(".btn13").on("click", saveText);
+$(".btn14").on("click", saveText);
+$(".btn15").on("click", saveText);
+$(".btn16").on("click", saveText);
+$(".btn17").on("click", saveText);
+
 let currentHour = moment().hour();
 
 $('.time-block').each( function(){
@@ -22,7 +42,14 @@ $('.time-block').each( function(){
     }
 });
 
-// save button
-function saveText(); {
-    localStorage.setItem(
-
+function saveText(){
+localStorage.a = $("#9oc").val();
+localStorage.b = $("#10oc").val();
+localStorage.c = $("#11oc").val();
+localStorage.d = $("#12oc").val();
+localStorage.e = $("#13oc").val();
+localStorage.f = $("#14oc").val();
+localStorage.g = $("#15oc").val();
+localStorage.h = $("#16oc").val();
+localStorage.i = $("#17oc").val();
+}
