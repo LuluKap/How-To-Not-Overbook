cd436a2f214615a8cea0eb1df97edb93c075086e
Lulu Kapita <lulukapita@gmail.com>
2022-07-04 20:29:17 -0400

    textarea color script

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 setInterval (() => {
     var time = moment().format("MMM Do YYYY,h:mm:ss a");
-    $("#currentDay").text(time);
+    $("#time").text(time);
     },1000)
+
+// time block colors
+//stackoverflow ftw
+
+let currentHour = moment().hour();
+
+$('.time-block').each( function(){
+    let block = parseInt($(this).attr('id'));
+    if(block > currentHour){
+        $(this).addClass('future');
+    }else if(block < currentHour){
+        $(this).addClass('past');
+        $(this).removeClass('future');
+    }else { (val === currentHour)
+        $(this).addClass('present');
+        $(this).removeClass('future');
+        $(this).removeClass('past');
+    }
+});
+
+// save button
+function saveText(); {
+    localStorage.setItem(
 
